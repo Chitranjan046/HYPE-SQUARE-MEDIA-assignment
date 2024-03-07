0b66abae3747143d82cfd68c82715c298cd67076
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-03-07 16:00:04 +0530

    Done login functionality

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
 export default function FooterCom() {
   return (
-    <Footer container className='border border-t-8 border-teal-500'>
+    <Footer container className='border border-t-8 bg-gradient-to-bl from-teal-300 via-pink-300 to-blue-300'>
       <div className='w-full max-w-7xl mx-auto'>
         <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
           <div className='mt-5'>
@@ -11,7 +11,7 @@ export default function FooterCom() {
               to='/'
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
-              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+              <span className='font-impact text-shadow-lg leading-normal tracking-wider'>
                 <i>HYPE SQUARE MEDIA ..!</i>
               </span>
 
