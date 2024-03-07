1841396d772c308dadc3e445c1654b31110469f6
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-03-07 15:07:29 +0530

    Done Signup  routes , Controller and adding a middleware and a function to handle the Error

--- server/index.js
+++ server/index.js
@@ -2,12 +2,30 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoute.js';
+import authRoutes from './routes/authRoute.js';
 const app = express();
 
 connectDB();
 app.use(express.json());
 
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
+// error handler
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
+
+  // Starting the server
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
